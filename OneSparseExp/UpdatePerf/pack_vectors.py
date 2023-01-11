f0807e57677a5bbcd1f37e3eceb499641b7724a4
Komorebi <cyq0919@mail.ustc.edu.cn>
2023-01-11 12:25:56 +0800

    fix bug

--- OneSparseExp/UpdatePerf/pack_vectors.py
+++ OneSparseExp/UpdatePerf/pack_vectors.py
@@ -3,33 +3,30 @@ import pickle
 import numpy as np
 import argparse
 
+suffix = ["841K", "1.77M", "2.65M", "3.54M", "4.42M", "5.31M", "6.19M", "7.07M", "7.96M", "8.84M"]
+length = [884183, 1768366, 2652549, 3536732, 4420915, 5305098, 6189281, 7073464, 7957647, 8841823]
 
-def pack_corpus(corpus_path, corpus_bin_path, corpus_base_bin_path):
-    embeddings = []
-    fvec = open(corpus_bin_path, "wb")
-    fvec.write(struct.pack("i", 8841823))   # number of documents
-    fvec.write(struct.pack("i", 769))       # dimension of vectors
-    for i in range(10):
-        with open(corpus_path + "%d.pt" % i, "rb") as f:
-            embedding, _ = pickle.load(f)
-            embeddings.append(embedding)
 
-            length = embedding.shape[0]
-            embedding_flattern = embedding.flatten()
-            fvec.write(struct.pack("%sf" % (length*769), *embedding_flattern))
-            print("loaded split %d, length = %d" % (i, length))
-    fvec.close()
-    print("finish corpus write")
-    embeddings = np.vstack(embeddings)
-    print(embeddings.shape)
+def pack_corpus(passage_path_prefix, corpus_bin_path_prefix, start=0, end=10):
+    for i in range(start, end):
+        print("start paking corpus %s" % suffix[i])
+        embeddings = []
+        fvec = open(corpus_bin_path_prefix + "%s.bin" % suffix[i], "wb")
+        fvec.write(struct.pack("i", length[i]))     # number of documents
+        fvec.write(struct.pack("i", 769))           # dimension of vectors
+        total_size = 0
+        for j in range(i+1):
+            with open(passage_path_prefix + "%d.pt" % j, "rb") as f:
+                embedding, _ = pickle.load(f)
+                embeddings.append(embedding)
 
-    fvec = open(corpus_base_bin_path, "wb")
-    fvec.write(struct.pack("i", 841823))    # number of documents
-    fvec.write(struct.pack("i", 769))       # dimension of vectors
-    embedding_flattern = embeddings[:841823].flatten()
-    fvec.write(struct.pack("%sf" % (841823*769), *embedding_flattern))
-    fvec.close()
-    print("finish corpus base write")
+                size = embedding.shape[0]
+                total_size += size
+                embedding_flattern = embedding.flatten()
+                fvec.write(struct.pack("%sf" % (size*769), *embedding_flattern))
+            print("pack %d" % j)
+        fvec.close()
+        print("finish corpus %d, size = %d" % (i, total_size))
 
 
 def pack_query(query_path, query_bin_path):
@@ -38,21 +35,21 @@ def pack_query(query_path, query_bin_path):
     fvec.write(struct.pack("i", 769))       # dimension of vectors
     with open(query_path, "rb") as f:
         embedding, _ = pickle.load(f)
-        #flattern embedding
+        # flattern embedding
         embedding_flattern = embedding.flatten()
         #write in bytes
         fvec.write(struct.pack("%sf" % (6980*769), *embedding_flattern))
     fvec.close()
 
 
-# use to verify
 def unpack(path):
     with open(path, "rb") as f:
         num_doc = struct.unpack("i", f.read(4))[0]
         dim = struct.unpack("i", f.read(4))[0]
         print(num_doc, dim)
-        queries = np.frombuffer(f.read(num_doc * dim * 4), dtype=np.float32).reshape((num_doc, dim))
-        print(queries)
+        embeddings = np.frombuffer(f.read(num_doc * dim * 4), dtype=np.float32).reshape((num_doc, dim))
+        print(embeddings.shape)
+        # print(embeddings)
         f.close()
 
 
@@ -62,13 +59,14 @@ if __name__ == "__main__":
                         help='prefix of path to passage embeddings')
     parser.add_argument('--query-path', type=str, default="../embedding_data/query/query_dev_small.pt",
                         help='path to query embeddings')
-    parser.add_argument('--corpus-bin-path', type=str, default="doc_vectors.bin",
-                        help='path to bin data of passages(8.8M)')
-    parser.add_argument('--corpus-base-bin-path', type=str, default="doc_vectors_base.bin",
-                        help='path to bin data of base passages(800K)')
-    parser.add_argument('--query-bin-path', type=str, default="query_vectors.bin",
+    parser.add_argument('--corpus-bin-path-prefix', type=str, default="./doc_vectors_",
+                        help='prefix of path to bin data of passages')
+    parser.add_argument('--query-bin-path', type=str, default="./query_vectors.bin",
                         help='path to to bin data of queries')
     args = parser.parse_args()
 
     pack_query(args.query_path, args.query_bin_path)
-    pack_corpus(args.passage_path_prefix, args.corpus_bin_path, args.corpus_base_bin_path)
+    unpack(args.query_bin_path)
+    pack_corpus(args.passage_path_prefix, args.corpus_bin_path_prefix)
+    for i in range(10):
+        unpack(args.corpus_bin_path_prefix + "%s.bin" % suffix[i])
